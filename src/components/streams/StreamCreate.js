11e690c5c83422566a630f49aeb22f003b4af066
victorgh <victor.gonzalez@soprasteria.com>
2019-05-21 15:11:03 +0200

    feat:  creating forms with redux form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+    renderInput({ input, label }) {
+        return (
+            <div className="field">
+                <label>{label}</label>
+                <input {...input} />
+            </div>
+        );
+    }
 
-export default StreamCreate;
+    onSubmit(formProps) {
+
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
+                <Field name="title" component={this.renderInput} label="Enter title" />
+                <Field name="description" component={this.renderInput} label="Enter description" />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({
+    form: 'streamCreate'
+})(StreamCreate);
